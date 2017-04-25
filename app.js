eecba957f9aec39be2e506d29485e52b695b5b95
christylaguardia <christinelaguardia@gmail.com>
2017-04-24 21:04:38 -0700

    Got the article template to work

---
+++ app.js
@@ -0,0 +1,35 @@
+// array that will hold all the blog article objects
+var articles = [];
+
+// constructor function
+function Article(blogObject) {
+  this.title = blogObject.title;
+  this.image = blogObject.image;
+  this.body = blogObject.body;
+}
+
+Article.prototype.toHtml = function() {
+  // clone the template html
+  var newArticle = $('article.template').clone();
+  // remove the template class
+  newArticle.removeClass('template');
+  // add the article data to the cloned template
+  newArticle.find('.article-title').text(this.title);
+  newArticle.find('.article-picture').children('img').attr('src', this.image);
+  newArticle.find('.article-body').html(this.body);
+  console.log("Created new Article object", newArticle)
+
+  return newArticle;
+}
+
+// run the blog data through the constructor function
+blogData.forEach(function(dataObject) {
+  articles.push(new Article(dataObject));
+  console.log("Added article to array");
+});
+
+// add each blog article to the page 
+articles.forEach(function(something) {
+  $('#blogContainer').append(something.toHtml());
+  console.log("Added new article to page");
+});
---
+++ articles.js
@@ -0,0 +1,19 @@
+var blogData = [];
+
+blogData = [
+  {
+    title: "Article Title 1",
+    image: "https://images.pexels.com/photos/59523/pexels-photo-59523.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb",
+    body: "<p>Puppy kitty ipsum dolor sit good dog stripes string. Speak licks kitten gimme five cage litter box bird water catch hamster. Ball litter box bark bird seed run fast Buddy wag tail. Furry parakeet play dead chew groom twine hamster fluffy biscuit dog chew.</p><p>Harness nest cat bird pet food kitten fish throw roll over bird seed. Hamster aquarium foot brush canary dinnertime bird food licks water paws lazy dog vaccine kitty carrier walk cat running cat.</p><p>Cat water cage Mittens chew slobber puppy Rover dinnertime meow scratcher run teeth dinnertime Rover toy ball water dog vaccination nest. Stripes leash food throw slobbery Snowball vaccine scratcher twine fleas walk tail running kisses collar kitty wag tail feeder left paw cage. Sit string water cage wagging bird seed litter box ferret aquarium ball chew commands drool twine good boy toy running cage. Fur scratcher Spike heel dinnertime scratch window lazy cat roll over.</p>"
+  },
+  {
+    title: "Article Title 2",
+    image: "https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb",
+    body: "<p>Puppy kitty ipsum dolor sit good dog stripes string. Speak licks kitten gimme five cage litter box bird water catch hamster. Ball litter box bark bird seed run fast Buddy wag tail. Furry parakeet play dead chew groom twine hamster fluffy biscuit dog chew.</p><p>Harness nest cat bird pet food kitten fish throw roll over bird seed. Hamster aquarium foot brush canary dinnertime bird food licks water paws lazy dog vaccine kitty carrier walk cat running cat.</p><p>Cat water cage Mittens chew slobber puppy Rover dinnertime meow scratcher run teeth dinnertime Rover toy ball water dog vaccination nest. Stripes leash food throw slobbery Snowball vaccine scratcher twine fleas walk tail running kisses collar kitty wag tail feeder left paw cage. Sit string water cage wagging bird seed litter box ferret aquarium ball chew commands drool twine good boy toy running cage. Fur scratcher Spike heel dinnertime scratch window lazy cat roll over.</p>"
+  },
+  {
+    title: "Article Title 3",
+    image: "https://images.pexels.com/photos/97863/pexels-photo-97863.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb",
+    body: "<p>Puppy kitty ipsum dolor sit good dog stripes string. Speak licks kitten gimme five cage litter box bird water catch hamster. Ball litter box bark bird seed run fast Buddy wag tail. Furry parakeet play dead chew groom twine hamster fluffy biscuit dog chew.</p><p>Harness nest cat bird pet food kitten fish throw roll over bird seed. Hamster aquarium foot brush canary dinnertime bird food licks water paws lazy dog vaccine kitty carrier walk cat running cat.</p><p>Cat water cage Mittens chew slobber puppy Rover dinnertime meow scratcher run teeth dinnertime Rover toy ball water dog vaccination nest. Stripes leash food throw slobbery Snowball vaccine scratcher twine fleas walk tail running kisses collar kitty wag tail feeder left paw cage. Sit string water cage wagging bird seed litter box ferret aquarium ball chew commands drool twine good boy toy running cage. Fur scratcher Spike heel dinnertime scratch window lazy cat roll over.</p>"
+  },
+]
