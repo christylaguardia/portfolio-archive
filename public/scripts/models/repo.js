7dc2cae975799f7a42fc8b7bb20dc44b2f54e1e7
christylaguardia <christinelaguardia@gmail.com>
2017-06-03 02:09:11 -0700

    Class 12

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -23,8 +23,13 @@
     )
   };
 
-  // filter repos for a specific attribute
-  repos.with = attr => repos.all.filter(repo => repo[attr]);
+  // filter repos for ones that have a description
+  repos.with = attr => repos.all.filter(repo => repo["description"])
+  // TODO: 
+  .sort(function(a,b) {
+    return (new Date(a.updated_at)) - (new Date(b.updated_at));
+  });;
+
 
   module.repos = repos;
 })(window);
