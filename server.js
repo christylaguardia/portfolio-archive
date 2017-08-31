df710dae96159eaf357dd88df3212a8495f98929
christylaguardia <christinelaguardia@gmail.com>
2017-08-31 01:43:37 -0700

    cleanup

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-'use strict';
-
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const urlEncodedParser = bodyParser.urlencoded( { extended: true });
 
 app.use(express.static('./public'));
 
